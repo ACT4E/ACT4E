aa675bfbdec357bb4721a7a5907d1b866bd91a02
Andrea Censi <acensi@ethz.ch>
2021-10-06 11:05:33 +0200

    changed alignment strategy

--- latexindent-changes-what.sh
+++ latexindent-changes-what.sh
@@ -3,13 +3,14 @@ tmp=.tmpfile
 for a in `git diff --name-only `; do
 # for a in `git diff --name-only --cached`; do
 echo $a;
-    if [[ $a =~ '.tex' ]];
+   if [[ $a == *".tex" ]];
     then
+    # echo "$a is  a tex file"
     cp $a $tmp;
     ./latexindent.sh $a;
-diff $tmp $a;
-else
-echo "Not a tex file"
+    diff $tmp $a;
+    else
+    echo "$a is not a tex file"
 fi
 done
 
