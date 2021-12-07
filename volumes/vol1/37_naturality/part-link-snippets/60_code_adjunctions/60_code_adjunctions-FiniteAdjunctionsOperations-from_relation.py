4297e0ee35a61a4613749c49d760414b3c9e4722
Gioele Zardini <gzardini@student.ethz.ch>
2021-12-07 10:31:10 +0100

    fix

---
+++ volumes/vol1/37_naturality/part-link-snippets/60_code_adjunctions/60_code_adjunctions-FiniteAdjunctionsOperations-from_relation.py
@@ -0,0 +1,1 @@
+from act4e_tex import write_source_minted; from act4e_interfaces import FiniteAdjunctionsOperations; write_source_minted(FiniteAdjunctionsOperations.from_relation, nodec=False,boxit=False,reduce_space=True)
