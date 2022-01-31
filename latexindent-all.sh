3663a63ec547e845da6a8e179dd30a9931343dcb
Andrea Censi <acensi@ethz.ch>
2022-01-31 20:31:41 +0100

    latexindent

--- latexindent-all.sh
+++ latexindent-all.sh
@@ -1,9 +1,9 @@
 #!/bin/zsh
 
-# for a in volumes/vol1/**/*.tex;
-#     do echo $a;
-#     ./latexindent.sh $a &  # Everything  in parallel
-# done
+for a in volumes/vol1/**/*.tex;
+    do echo $a;
+    ./latexindent.sh $a &  # Everything  in parallel
+done
 
 for a in sag/*.tikz;
     do echo $a;
