be7e075fc4bc92c9c2b6455185bedb5233993721
Andrea Censi <acensi@ethz.ch>
2022-10-07 14:42:33 +0200

    misc

--- volumes/vol1/37_naturality/part-link-snippets/80_code_exercise/80_code_exercise-FiniteNaturalTransformationRepresentation.py
+++
@@ -1,1 +0,0 @@
-from act4e_tex import write_source_minted; from act4e_interfaces import FiniteNaturalTransformationRepresentation; write_source_minted(FiniteNaturalTransformationRepresentation, nodec=False,boxit=False,reduce_space=True);
--- volumes/vol1/37_naturality/part-link-snippets/80_code_exercise/80_code_exercise-nat1.py
+++
@@ -1,1 +0,0 @@
-from act4e_tex import visualize_data_file;visualize_data_file('nat1');
