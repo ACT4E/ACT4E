6d5dfe0d36406726c906e668b93d20a3c1d95b87
JonathanLorand <jonathanlorand@gmail.com>
2026-06-11 15:03:36 +0200

    added all todos from this past monday's discussion; implemented many edits

---
+++ volumes/vol1/10_arrows/part-link-snippets/80_exercise/80_exercise-cat_single.py
@@ -0,0 +1,1 @@
+from act4e_tex import visualize_data_file;visualize_data_file('cat_single',breakat={'morphisms:': '\\vfill\\columnbreak', 'equations:': '\\vfill\\columnbreak'});
---
+++ volumes/vol1/10_arrows/part-link-snippets/90_exercises_currency/90_exercises_currency-cat_cur_example1.py
@@ -0,0 +1,1 @@
+from act4e_tex import visualize_data_file;visualize_data_file('cat_cur_example1', min_size=10, max_size=30);
