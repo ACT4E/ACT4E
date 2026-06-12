cdb95d3b01d958b946371e1ff2ff51179537dcbc
JonathanLorand <jonathanlorand@gmail.com>
2026-06-12 11:41:02 +0200

    more passages into devel

---
+++ volumes/vol1/25_translation/part-link-snippets/50_functor_code/50_functor_code-FiniteFunctor.py
@@ -0,0 +1,1 @@
+from act4e_tex import write_source_minted; from act4e_interfaces import FiniteFunctor; write_source_minted(FiniteFunctor, nodec=False,boxit=False,reduce_space=True);
---
+++ volumes/vol1/25_translation/part-link-snippets/50_functor_code/50_functor_code-FiniteFunctorRepresentation.py
@@ -0,0 +1,1 @@
+from act4e_tex import write_source_minted; from act4e_interfaces import FiniteFunctorRepresentation; write_source_minted(FiniteFunctorRepresentation, nodec=False,boxit=False,reduce_space=True);
