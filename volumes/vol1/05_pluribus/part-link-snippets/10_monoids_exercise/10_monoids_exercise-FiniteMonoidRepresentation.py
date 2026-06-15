330496ea0372172916542a6c2fa4bf0be9ae088b
JonathanLorand <jonathanlorand@gmail.com>
2026-06-15 11:49:40 +0200

    created 'new from old' chapter for algebra part

---
+++ volumes/vol1/05_pluribus/part-link-snippets/10_monoids_exercise/10_monoids_exercise-FiniteMonoidRepresentation.py
@@ -0,0 +1,1 @@
+from act4e_tex import write_source_minted; from act4e_interfaces import FiniteMonoidRepresentation; write_source_minted(FiniteMonoidRepresentation, nodec=False,boxit=False,reduce_space=True);
---
+++ volumes/vol1/05_pluribus/part-link-snippets/15_rope_goldberg/15_rope_goldberg-Solution.py
@@ -0,0 +1,1 @@
+from act4e_tex import write_source_minted; from act4e_interfaces import Solution; write_source_minted(Solution, nodec=False,boxit=False,reduce_space=True);
