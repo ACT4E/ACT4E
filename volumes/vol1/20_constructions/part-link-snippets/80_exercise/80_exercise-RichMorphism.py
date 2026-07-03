04757ceb5c0b971eb43271e55779793d8f6bda45
JonathanLorand <jonathanlorand@gmail.com>
2026-07-03 17:13:55 +0200

    restructuring the book based on today's discussion

---
+++ volumes/vol1/20_constructions/part-link-snippets/80_exercise/80_exercise-RichMorphism.py
@@ -0,0 +1,1 @@
+from act4e_tex import write_source_minted; from act4e_interfaces import RichMorphism; write_source_minted(RichMorphism, nodec=False,boxit=False,reduce_space=True);
---
+++ volumes/vol1/20_constructions/part-link-snippets/80_exercise/80_exercise-cat_Zero.py
@@ -0,0 +1,1 @@
+from act4e_tex import visualize_data_file;visualize_data_file('cat_Zero',breakat={'morphisms:': '\\vfill\\columnbreak', 'equations:': '\\vfill\\columnbreak'});
